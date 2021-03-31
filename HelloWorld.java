36138ceffcb0d52f8964797427821c1aa6d6ad90
EC2 Default User <ec2-user@ip-172-31-47-194.ap-south-1.compute.internal>
2021-03-31 12:05:13 +0000

    created webhooks 5th tril

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, hi hello guy wts up lest go to party  today night d");
+    System.out.println("Hello, hi hello guy wts up lest go to partynow i created webhooks  today night d");
   }
 }
